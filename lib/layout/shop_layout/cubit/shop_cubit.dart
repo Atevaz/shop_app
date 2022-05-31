0627d4b25ca43c8f0a53fba6056f0d21fa53e489
Atevaz <101355785+Atevaz@users.noreply.github.com>
2022-05-31 13:33:38 +0200

    all

---
+++ lib/layout/shop_layout/cubit/shop_cubit.dart
@@ -0,0 +1,52 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:todo_app/layout/shop_layout/cubit/shop_states.dart';
+
+import '../../../models/shop_app/home_model.dart';
+import '../../../modules/shop_app/categorey/categorey_screen.dart';
+import '../../../modules/shop_app/favorites/favorites_screen.dart';
+import '../../../modules/shop_app/proudcts/products_screen.dart';
+import '../../../modules/shop_app/settings/settings_screen.dart';
+import '../../../shared/network/end_point.dart';
+import '../../../shared/network/remote/dio_helper.dart';
+
+class ShopCubit extends Cubit <ShopStates> {
+
+  ShopCubit() : super(ShopInitialState());
+
+  static ShopCubit get(context) => BlocProvider.of(context);
+
+  int currentIndex = 0 ;
+
+  List <Widget> BottomScreens = [
+    ProductsScreen(),
+    CategoreyScreen(),
+    FavoritesScreen(),
+    SettingScreen(),
+  ] ;
+
+  void changeNavBar (index) {
+    currentIndex = index ;
+    emit(ShopChangeNavBarState());
+  }
+
+  HomeModel? homeModel ;
+
+  void getHomeData (){
+    emit(ShopLoadingGetHomeDataState());
+    DioHelper.getData(url: HOME).then((value){
+
+      homeModel = HomeModel.fromJson(value.data);
+
+      print(homeModel.toString());
+
+      emit(ShopSuccessGetHomeDataState());
+    }).catchError((error){
+      print(error.toString());
+      emit(ShopErrorGetHomeDataState());
+    });
+  }
+
+}
---
+++ lib/modules/shop_app/categorey/categorey_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class CategoreyScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Container(child: Text('Categorey')));
+  }
+}
